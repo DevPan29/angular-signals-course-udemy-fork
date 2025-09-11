19229bb628abd394a474925bc5460db10df7431f
Alessandro Panarese <darenagel@hotmail.it>
2025-09-11 07:32:32 +0200

    master detail pattern

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -13,7 +13,17 @@ import {LessonDetailComponent} from "./lesson-detail/lesson-detail.component";
 })
 export class LessonsComponent {
 
+  mode = signal<'master' | 'detail'>("master");
 
+  lessons = signal<Lesson[]>([]);
 
+  selectedLesson = signal<Lesson | null>(null);
 
+  lessonService = inject(LessonsService);
+
+  searchInput = viewChild<ElementRef>('search');
+
+  onSearch() {
+
+  }
 }
